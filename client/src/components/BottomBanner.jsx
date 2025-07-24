fc11987fec2cbf840fee0473a729a12e24f1570b
Priyatam Shaw <priyatamshaw@gmail.com>
2025-07-24 15:16:15 +0530

    updated payment option

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -9,13 +9,13 @@ const BottomBanner = () => {
         <img src={assets.bottom_banner_image_sm} alt="" className='w-full md:hidden'/>
         <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-20 lg:pr-52'>
           <div>
-            <h1 className='text-2xl md:text-3xl font-semibold text-green-500 mb-6'>Why we are the Best ?</h1>
+            <h1 className='text-2xl md:text2xl lg:text-3xl font-semibold text-green-500 mb-6 md:mb-0'>Why we are the Best ?</h1>
             {features.map((feature, index)=>(
               <div key={index} className='flex items-center gap-4 mt-2'>
-                <img src={feature.icon} alt="" className='md:w-11 w-9'/>
+                <img src={feature.icon} alt="" className='md:w-9 lg:w-11 w-9'/>
                 
                 <div>
-                <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
+                <h3 className='text-lg md:text-sm lg:text-xl font-semibold'>{feature.title}</h3>
                 <p className='text-gray-500/70 text-xs md:text-sm'>{feature.description}</p>
               </div>
                 </div>
--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -20,7 +20,7 @@ const Testimonial = () => {
                 <p className="text-sm md:text-base text-gray-500/90 mt-2 max-w-[696px]">Hear what our customers say about us. We're always looking for ways to improve. If you have a positive experience with us, leave a review.</p>
             </div>
 
-            <div className="flex flex-wrap items-center justify-center gap-6 mt-20 mb-10">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-6 mt-20 mb-10">
                 {testimonials.map((testimonial) => (
                     <div key={testimonial.id} className="bg-white p-6 rounded-xl shadow max-w-xs">
                         <div className="flex items-center gap-3">
